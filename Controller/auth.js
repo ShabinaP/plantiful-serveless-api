47497adde09f076b2f6405c01abc1fc5594d389c
mahmoud jeilani <jeilani@gmail.com>
2021-12-13 17:39:47 +0000

    added user login in

--- Controller/auth.js
+++ Controller/auth.js
@@ -6,31 +6,40 @@ const bcrypt = require("bcrypt");
 const dbConnection = require('../dbConfigs');
 const userServices = require('../Services/auth');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const genWebToken = require('../Services/genwebToken')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 //  function for creating a new user
 app.post('/auth/register', async (req, res) => {
   try {
-   await dbConnection();
-  const salt = await bcrypt.genSalt(10);
-  const {username,password,email,name} = req.body;
-  const encryptedPassword = await bcrypt.hash(password, salt);
-  console.log(username)
-  const data = {
-    username,
-    email,
-    name,
-    password: encryptedPassword
-  }
- if(!data) {
-     return "Please pass all required fields!"
-  }
-  
-   let createUser=  await userServices.createUser(data);
-   if (createUser) {
-     return res.status(200).send(
-      createUser
-    )
-   }
+    await dbConnection();
+    const {
+      password,
+      email,
+      name
+    } = req.body;
+    const data = {
+      email,
+      name,
+      password
+    }
+    if (!data) {
+      return "Please pass all required fields!"
+    }
+
+    let createUser = await userServices.createUser(data);
+    if (createUser) {
+      const {
+        password,
+        ...others
+      } = createUser
+
+      return res.status(200).send(
+        others
+      )
+    }
   } catch (error) {
     //  handle errors here
     console.log(error, "error!!");
@@ -39,59 +48,80 @@ app.post('/auth/register', async (req, res) => {
 
 
 app.get('/auth/:id', async (req, res) => {
-   try {
-     await dbConnection();
-     const userid = req.params.id
-     const getUser = await userServices.getUser(userid)
-     if(getUser){
-       return res.status(200).send(getUser)
-      
-     }
-     
-   } catch (error) {
-     
-   }
-})
-
-app.put('/auth/update/:id', async (req, res)=>{
-  if(req.body.userid===req.params.id){
-    if(req.body.password) {
-      const salt = await bcrypt.genSalt(10)
-      req.body.password = await bcrypt.hash(req.body.password, salt)
-    }
-  
   try {
     await dbConnection();
     const userid = req.params.id
-    const updateField = req.body
-    const updatedUSer = await userServices.updatedUser(userid, updateField)
-    
-    if(updatedUSer){
-      return res.status(200).send(updatedUSer)
+    const getUser = await userServices.getUser(userid)
+    if (getUser) {
+      return res.status(200).send(getUser)
+
     }
+
   } catch (error) {
-    console.log(error)
+
   }
-}
 })
-app.delete('/auth/delete/:id', async (req, res)=>{
-  if(req.body.userid===req.params.id){
+
+app.put('/auth/update/:id', async (req, res) => {
+  if (req.body.userid === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10)
+      req.body.password = await bcrypt.hash(req.body.password, salt)
+    }
+
     try {
-    await dbConnection();
-    const userid = req.params.id
-    const deletedtedUSer = await userServices.deletedUser(userid)
-    
-    if(deletedtedUSer){
-      return res.status(200).send(deletedtedUSer)
+      await dbConnection();
+      const userid = req.params.id
+      const updateField = req.body
+      const updatedUSer = await userServices.updatedUser(userid, updateField)
+
+      if (updatedUSer) {
+        return res.status(200).send(updatedUSer)
+      }
+    } catch (error) {
+      console.log(error)
     }
-  } catch (error) {
-    console.log(error)
   }
-}
-return res.status(401).json("Not allowed")
+})
+app.delete('/auth/delete/:id', async (req, res) => {
+  if (req.body.userid === req.params.id) {
+    try {
+      await dbConnection();
+      const userid = req.params.id
+      const deletedtedUSer = await userServices.deletedUser(userid)
+
+      if (deletedtedUSer) {
+        return res.status(200).send(deletedtedUSer)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return res.status(401).json("Not allowed")
 })
 
+app.post('/auth/login', async (req, res) => {
+  const {
+    email,
+    password
+  } = req.body
+  console.log(email)
+  try {
+    await dbConnection();
+    const userLogin = await userServices.userLogin(email, password)
+    if (userLogin) {
+      return res.status(200).send(userLogin)
+    }
+  } catch (error) {
+    return res.status(200).json({
+      message: error.message
+    })
 
+  }
+return res.status(401).send('Not Validated')
+
+
+})
 
 
 
--- Controller/plants.js
+++ Controller/plants.js
@@ -7,7 +7,9 @@ const dbConnection = require('../dbConfigs');
 const plantServices = require('../Services/plants');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
 
 //  base url to test our API
 app.get('/health/', async (req, res) => {
@@ -17,41 +19,41 @@ app.get('/health/', async (req, res) => {
 // push plant into user plants array
 //adding plant into user plants collection 
 app.put('/plant/add/', async (req, res) => {
-  try {
-   await dbConnection();
+   try {
+      await dbConnection();
 
-   const userId = req.body.userId
-   const plantId = req.body.plantId
+      const userId = req.body.userId
+      const plantId = req.body.plantId
 
-   let user=  await plantServices.pushPlantToUser(userId, plantId);
- 
-   if (user) {
-     return res.status(200).send({
-        user
-     }
-      
-    )
+      let user = await plantServices.pushPlantToUser(userId, plantId);
+
+      if (user) {
+         return res.status(200).send({
+               user
+            }
+
+         )
+      }
+   } catch (error) {
+      //  handle errors here
+      console.log(error, "error!!");
    }
-  } catch (error) {
-    //  handle errors here
-    console.log(error, "error!!");
-  }
 })
 
 //  function for getting all plants
 app.get('/plants/', async (req, res) => {
-try {
-    await dbConnection();
-    const allPlants = await plantServices.getAllPlants();
-    if (allPlants) {
-      return res.status(200).send({
-        data: allPlants
-      })
-    }
-  } catch (error) {
-     //  handle errors here
-     console.log(error, "error!!");
-  }
+   try {
+      await dbConnection();
+      const allPlants = await plantServices.getAllPlants();
+      if (allPlants) {
+         return res.status(200).send({
+            data: allPlants
+         })
+      }
+   } catch (error) {
+      //  handle errors here
+      console.log(error, "error!!");
+   }
 })
 
 //search plant by familyName/ can add more search params  
@@ -62,56 +64,56 @@ app.get('/plant/:plantname', async (req, res) => {
       await dbConnection();
       const request = req.params.plantname
       console.log(request)
-       const plantname = new RegExp(request, "i");
+      const plantname = new RegExp(request, "i");
       console.log(request)
       const plant = await plantServices.getPlantByName(plantname);
-      if(plant){
+      if (plant) {
          return res.status(200).send({
             data: plant
          })
       }
-   
+
 
 
    } catch (error) {
-      
+
    }
 })
 
 
 //get all plants for one particular user (works)  
- // fetches user plants from dataase using userPlant array 
- // this same method can be used to fetch user wish list from database
-app.get('/plants/:userplants', async (req, res)=>{
+// fetches user plants from dataase using userPlant array 
+// this same method can be used to fetch user wish list from database
+app.get('/plants/:userplants', async (req, res) => {
    await dbConnection();
 
    const request = req.params.id
    const _id = request
    const userPlants = await plantServices.getUserPlants(_id);
-   if(userPlants){
+   if (userPlants) {
       return res.status(200).send({
          data: userPlants
       })
    }
 
 })
-app.put('/plants/delete/:userplant/', async (req, res)=>{
- try {
-   await dbConnection();
-   const userid = req.body.userid
-   const plantId = req.body.plantid
-   const deleteUserPlant = await plantServices.deleteUserPlant(userid, plantId)
-   if(deleteUserPlant){
-      return res.status(200).send({
-         data: deleteUserPlant
+app.put('/plants/delete/:userplant/', async (req, res) => {
+   try {
+      await dbConnection();
+      const userid = req.body.userid
+      const plantId = req.body.plantid
+      const deleteUserPlant = await plantServices.deleteUserPlant(userid, plantId)
+      if (deleteUserPlant) {
+         return res.status(200).send({
+            data: deleteUserPlant
+         })
+      }
+   } catch (error) {
+      res.status(500).send({
+         error: error.message
       })
+
    }
- } catch (error) {
-    res.status(500).send({
-       error: error.message
-    })
-    
- }
 })
 
 
@@ -119,54 +121,6 @@ app.put('/plants/delete/:userplant/', async (req, res)=>{
 
 
 
-// //  function for getting a  product by Id
-// app.get('/plant/:productId/', async (req, res) => {
-//   try {
-//     await dbConnection();
-//     const {productId} = req.params;
-//     const getPlant = await ProductService.getPlantByName({productId});
-//     if(getPlant) {
-//       return res.status(200).send({
-//         data: getPlant
-//       })
-//     }
-//   } catch (error) {
-//      //  handle errors here
-//      console.log(error, "error!!");
-//   }
-// });
 
-
-// app.post('/auth/register', async (req, res) => {
-//   try {
-//    await dbConnection();
-//    const salt = await bcrypt.genSalt(10);
-//   const password = await bcrypt.hash(req.body.password, salt);
-//   const username = req.body.username
-//   const email = req.body.email
-//   const data = {
-//     password,
-//     username,
-//     email,
-//   }
-//  if(!data) {
-//      return "Please pass all required fields!"
-//   }
-//    const userData = {
-//     password,
-//     username,
-//     email,
-//    };
-//    let createUser=  await ProductService.createUser(userData);
-//    if (createUser) {
-//      return res.status(200).send(
-//        createUser
-//     )
-//    }
-//   } catch (error) {
-//     //  handle errors here
-//     console.log(error, "error!!");
-//   }
-// })
 
 module.exports.handler = serverless(app);
